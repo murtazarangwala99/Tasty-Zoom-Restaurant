19d5a81c00b1780f2d457c45952a87369ab40f6e
murtazarangwala99 <murtazarangwala95@gmail.com>
2024-11-22 20:24:16 +0530

    Netlify CORS issue fixed

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -11,9 +11,9 @@ export const API_LINK =
   "/api/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MOBILE_API_LINK =
-  "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fmapi%2Fhomepage%2FgetCards%3Flat%3D21.1702401%26lng%3D72.83106070000001";
+  // "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fmapi%2Fhomepage%2FgetCards%3Flat%3D21.1702401%26lng%3D72.83106070000001";
 
-// https://www.swiggy.com/mapi/homepage/getCards?lat=21.1702401&lng=72.83106070000001
+  "/api/https://www.swiggy.com/mapi/homepage/getCards?lat=21.1702401&lng=72.83106070000001";
 
 export const MENU_API =
   // "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&restaurantId=";
@@ -27,7 +27,7 @@ export const MOBILE_MENU_API =
 
   // "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fmapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D21.1702401%26lng%3D72.83106070000001%26restaurantId=";
 
-  "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&restaurantId=100721&submitAction=ENTER";
+  "/api/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&restaurantId=100721&submitAction=ENTER";
 
 export const ITEM_IMAGE =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/";
